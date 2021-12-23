f6733424f15a04fdcc4199d81a4dbb13f647c7c8
Daniel Below <daniel.below@tum.de>
2021-12-23 12:20:01 +0100

    Add day23

---
+++ src/day23/mod.rs
@@ -0,0 +1,13 @@
+// Solved by hand
+
+use aoc_runner_derive::aoc;
+
+#[aoc(day23, part1)]
+pub fn part1(_: &str) -> u64 {
+    17120
+}
+
+#[aoc(day23, part2)]
+pub fn part2(_: &str) -> u64 {
+    47234
+}
